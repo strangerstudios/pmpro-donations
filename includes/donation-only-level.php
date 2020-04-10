5d7eeb868e916c4b976f152532e3132d0d0d85df
dparker1005 <dlparker1005@gmail.com>
2020-04-10 12:47:10 -0400

    Fixed donation only level not working for PayPal Express

--- includes/donation-only-level.php
+++ includes/donation-only-level.php
@@ -9,10 +9,11 @@
 /**
  * Set existing member flag before checkout.
  */
-function pmprodon_pmpro_checkout_before_change_membership_level() {
-	global $pmprodon_existing_member_flag, $pmpro_level;	
-	
+function pmprodon_pmpro_checkout_before_change_membership_level( $user_id, $morder ) {
+	global $pmprodon_existing_member_flag, $pmpro_level;
+
 	if ( pmpro_hasMembershipLevel()
+	&& pmpro_is_checkout()
 	&& ! empty( $pmpro_level )
 	&& pmprodon_is_donations_only( $pmpro_level->id ) ) {
 		add_filter( 'pmpro_cancel_previous_subscriptions', '__return_false' );
@@ -20,22 +21,22 @@ function pmprodon_pmpro_checkout_before_change_membership_level() {
 		$pmprodon_existing_member_flag = true;
 	}
 }
-add_action( 'pmpro_checkout_before_processing', 'pmprodon_pmpro_checkout_before_change_membership_level', 1 );
+add_action( 'pmpro_checkout_before_change_membership_level', 'pmprodon_pmpro_checkout_before_change_membership_level', 1, 2 );
 
 /**
  * Give existing users their old level back after checkout.
  */
 function pmprodon_pmpro_after_checkout( $user_id ) {
 	global $wpdb, $pmprodon_existing_member_flag, $pmpro_level;
-	
-	if( isset( $pmprodon_existing_member_flag ) ) {
-		//remove last row added to members_users table
+
+	if ( isset( $pmprodon_existing_member_flag ) ) {
+		// Remove last row added to members_users table.
 		$sqlQuery = "DELETE FROM $wpdb->pmpro_memberships_users WHERE user_id = '" . esc_sql( $user_id ) . "' AND membership_id = '" . esc_sql( $pmpro_level->id ) . "' ORDER BY id DESC LIMIT 1";
 		$wpdb->query( $sqlQuery );
-			
-		//reset user
+
+		// Reset user.
 		global $all_membership_levels;
-		unset( $all_membership_levels[$user_id] );
+		unset( $all_membership_levels[ $user_id ] );
 		pmpro_set_current_user();
 	}
 }
