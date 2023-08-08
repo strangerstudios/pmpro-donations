261b0f521902b5bf6afa71f96810d0be9e123fb7
maximilianoRicoTabo <maximilian.rico@gmail.com>
2023-08-08 11:08:42 -0300

    Displays Membership Costs 0 when only used for donations #43  * Add a filter to invoice bullets to let users customize

--- includes/checkout.php
+++ includes/checkout.php
@@ -48,6 +48,7 @@ function pmprodon_pmpro_checkout_after_level_cost() {
 	$max_price       = $donfields['max_price'];
 	$dropdown_prices = $donfields['dropdown_prices'];
 
+
 	if ( isset( $_REQUEST['donation'] ) ) {
 		$donation = preg_replace( '[^0-9\.]', '', $_REQUEST['donation'] );
 	} elseif ( isset( $_SESSION['donation'] ) ) {
@@ -315,10 +316,9 @@ add_filter( 'pmpro_checkout_order', 'pmprodon_pmpro_checkout_order' );
 function pmprodon_pmpro_invoice_bullets_bottom( $order ) {
 	$components = pmprodon_get_price_components( $order );
 	if ( ! empty( $components['donation'] ) ) {
-	?>
-	<li><strong><?php _e( 'Membership Cost', 'pmpro-donations' ); ?>: </strong> <?php echo pmpro_formatPrice( $components['price'] ); ?></li>
-	<li><strong><?php _e( 'Donation', 'pmpro-donations' ); ?>: </strong> <?php echo pmpro_formatPrice( $components['donation'] ); ?></li>
-	<?php
+		$bullets = '<li><strong>' . __( 'Membership Cost', 'pmpro-donations' ) . ": </strong> " . pmpro_formatPrice( $components['price'] ) . '</li>' 
+		 . '<li><strong>' . __( 'Donation', 'pmpro-donations' ) . ": </strong>"  .  pmpro_formatPrice( $components['donation'] ) . '</li>';
+	echo $bullets = apply_filters( 'pmpro_donations_bullets_invoice', $bullets, $order );
 	}
 }
 add_filter( 'pmpro_invoice_bullets_bottom', 'pmprodon_pmpro_invoice_bullets_bottom' );
